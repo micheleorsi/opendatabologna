c7316ffbb85b04593355059ec070d17a14ef741f
Michele Orsi <morsi@map2app.com>
2013-07-26 01:17:51 +0200

    alcuni fix minori

--- anagrafe_eletti/presenze_e_votazioni/html/visualization_votazioni.js
+++ anagrafe_eletti/presenze_e_votazioni/html/visualization_votazioni.js
@@ -9,16 +9,3 @@ function drawVisualization1() {
     		  });
     		  wrapper.draw();
       }
-
-//function drawVisualization2() {
-//    // Create and populate the data table.
-//	  var wrapper = new google.visualization.ChartWrapper({
-//		    chartType: 'ColumnChart',
-//		    dataTable: [nomi_presenze_question_time, presenze_presenze_question_time],
-//		    options: {'title': 'Percentuale votazioni consiglieri',
-//		    	'legend':'none'},
-//		    containerId: 'visualization2'
-//		  });
-//		  wrapper.draw();
-//  }
-
