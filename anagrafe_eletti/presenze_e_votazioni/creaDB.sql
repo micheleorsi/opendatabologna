d90bcbe33ffb6956f52e79641cb51045732026d7
Michele Orsi <morsi@map2app.com>
2013-09-25 22:37:06 +0200

    aggiornato con gli ultimi dati

--- anagrafe_eletti/presenze_e_votazioni/creaDB.sql
+++ anagrafe_eletti/presenze_e_votazioni/creaDB.sql
@@ -1,3 +1,3 @@
-create table PRESENZE_E_VOTAZIONI(Progressivo integer, Nominativo text, Data text, GruppoPolitico text, Presenza text, NumVotazioni integer, PercentualePresenza integer);
+create table PRESENZE_E_VOTAZIONI(Progressivo integer, Nominativo text, Data text, GruppoPolitico text, Presenza text, NumVotazioni integer);
 .separator ","
-.import cleaned/presenze_e_votazioni.csv PRESENZE_E_VOTAZIONI
+.import cleaned/presenze_e_votazioni_19_08_2013.csv PRESENZE_E_VOTAZIONI
