9063fb1e5df05ceba3f9f1d9ce1506c1b58feab4
Michele Orsi <morsi@map2app.com>
2013-09-25 23:58:00 +0200

    aggiunte query per carella, errani, aldrovandi e merola

--- anagrafe_eletti/presenze_e_votazioni/script.py
+++ anagrafe_eletti/presenze_e_votazioni/script.py
@@ -3,7 +3,37 @@ import json
 
 conn = sqlite3.connect('presenze_e_votazioni.db')
 
-# presenze suddivise per mese
+date_carella = [''];
+presenze_carella = [''];
+
+date_errani = [''];
+presenze_errani = [''];
+
+date_aldrovandi = [''];
+presenze_aldrovandi = [''];
+
+date_merola = [''];
+presenze_merola = [''];
+
+# presenze suddivise per mese CARELLA DANIELE
+for row in conn.execute('select strftime("%Y-%m",Data) AS DATA, COUNT(Presenza) AS TOTALE_PRESENZE from PRESENZE_E_VOTAZIONI WHERE Presenza = "P" AND strftime("%w",Data) == "1" AND NOMINATIVO == "CARELLA DANIELE" GROUP BY Progressivo, strftime("%Y-%m",Data) ORDER BY DATA ASC'):
+    date_carella.append(row[0])
+    presenze_carella.append(row[1])
+
+# presenze suddivise per mese ERRANI FRANCESCO
+for row in conn.execute('select strftime("%Y-%m",Data) AS DATA, COUNT(Presenza) AS TOTALE_PRESENZE from PRESENZE_E_VOTAZIONI WHERE Presenza = "P" AND strftime("%w",Data) == "1" AND NOMINATIVO == "ERRANI FRANCESCO" GROUP BY Progressivo, strftime("%Y-%m",Data) ORDER BY DATA ASC'):
+    date_errani.append(row[0])
+    presenze_errani.append(row[1])
+
+# presenze suddivise per mese ALDROVANDI STEFANO
+for row in conn.execute('select strftime("%Y-%m",Data) AS DATA, COUNT(Presenza) AS TOTALE_PRESENZE from PRESENZE_E_VOTAZIONI WHERE Presenza = "P" AND strftime("%w",Data) == "1" AND NOMINATIVO == "ALDROVANDI STEFANO" GROUP BY Progressivo, strftime("%Y-%m",Data) ORDER BY DATA ASC'):
+    date_aldrovandi.append(row[0])
+    presenze_aldrovandi.append(row[1])
+
+# presenze suddivise per mese MEROLA VIRGINIO
+for row in conn.execute('select strftime("%Y-%m",Data) AS DATA, COUNT(Presenza) AS TOTALE_PRESENZE from PRESENZE_E_VOTAZIONI WHERE Presenza = "P" AND strftime("%w",Data) == "1" AND NOMINATIVO == "MEROLA VIRGINIO" GROUP BY Progressivo, strftime("%Y-%m",Data) ORDER BY DATA ASC'):
+    date_merola.append(row[0])
+    presenze_merola.append(row[1])
 
 # presenze
 nomi_presenze_totali = [''];
@@ -52,29 +82,49 @@ for row in conn.execute('select Nominativo,SUM(NumVotazioni) AS TOTALE from PRES
 conn.close()
 
 with open('html/data_presenze.js', 'w') as outfile:
-    outfile.write('nomi_presenze_totali=')
-    json.dump(nomi_presenze_totali, outfile)
-    outfile.write(';\npresenze_presenze_totali=')
-    json.dump(presenze_presenze_totali, outfile)
+    outfile.write('date_carella=');
+    json.dump(date_carella, outfile);
+    outfile.write(';\npresenze_carella=');
+    json.dump(presenze_carella, outfile);
+
+    outfile.write(';\n\ndate_errani=');
+    json.dump(date_errani, outfile);
+    outfile.write(';\npresenze_errani=');
+    json.dump(presenze_errani, outfile);
+
+    outfile.write(';\n\ndate_aldrovandi=');
+    json.dump(date_aldrovandi, outfile);
+    outfile.write(';\npresenze_aldrovandi=');
+    json.dump(presenze_aldrovandi, outfile);
+
+    outfile.write(';\n\ndate_merola=');
+    json.dump(date_merola, outfile);
+    outfile.write(';\npresenze_merola=');
+    json.dump(presenze_merola, outfile);
     
-    outfile.write(';\n\nnomi_presenze_question_time=')
-    json.dump(nomi_presenze_question_time, outfile)
-    outfile.write(';\npresenze_presenze_question_time=')
-    json.dump(presenze_presenze_question_time, outfile)
+    outfile.write(';\n\nnomi_presenze_totali=');
+    json.dump(nomi_presenze_totali, outfile);
+    outfile.write(';\npresenze_presenze_totali=');
+    json.dump(presenze_presenze_totali, outfile);
     
-    outfile.write(';\n\nnomi_presenze_consiglio_ordinario=')
-    json.dump(nomi_presenze_consiglio_ordinario, outfile)
-    outfile.write(';\npresenze_presenze_consiglio_ordinario=')
-    json.dump(presenze_presenze_consiglio_ordinario, outfile)
+    outfile.write(';\n\nnomi_presenze_question_time=');
+    json.dump(nomi_presenze_question_time, outfile);
+    outfile.write(';\npresenze_presenze_question_time=');
+    json.dump(presenze_presenze_question_time, outfile);
     
-    outfile.write(';\n\nnomi_presenze_consiglio_straordinario=')
-    json.dump(nomi_presenze_consiglio_straordinario, outfile)
-    outfile.write(';\npresenze_presenze_consiglio_straordinario=')
-    json.dump(presenze_presenze_consiglio_straordinario, outfile)
-    outfile.write(';')
+    outfile.write(';\n\nnomi_presenze_consiglio_ordinario=');
+    json.dump(nomi_presenze_consiglio_ordinario, outfile);
+    outfile.write(';\npresenze_presenze_consiglio_ordinario=');
+    json.dump(presenze_presenze_consiglio_ordinario, outfile);
+    
+    outfile.write(';\n\nnomi_presenze_consiglio_straordinario=');
+    json.dump(nomi_presenze_consiglio_straordinario, outfile);
+    outfile.write(';\npresenze_presenze_consiglio_straordinario=');
+    json.dump(presenze_presenze_consiglio_straordinario, outfile);
+    outfile.write(';');
     
 with open('html/data_votazioni.js', 'w') as outfile:
-    outfile.write('nomi_votazioni_totali=')
-    json.dump(nomi_votazioni_totali, outfile)
-    outfile.write(';\npresenze_votazioni_totali=')
-    json.dump(presenze_votazioni_totali, outfile)
+    outfile.write('nomi_votazioni_totali=');
+    json.dump(nomi_votazioni_totali, outfile);
+    outfile.write(';\npresenze_votazioni_totali=');
+    json.dump(presenze_votazioni_totali, outfile);
